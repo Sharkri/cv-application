e2f5bb182ad1a23a016b8a4941726945ef30978a
Sharkri <sharkdude955@gmail.com>
2022-10-21 22:44:58 -0500

    created personaldetails section in resume

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import PersonalDetails from "./components/PersonalDetails";
 import Resume from "./components/Resume";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      personalDetails: {
+        fullName: "",
+        email: "beans@beans.com",
+        phoneNumber: "",
+        address: "",
+      },
+    };
+  }
+  personalDetailsChange = (e) => {};
+
   render() {
+    const { personalDetails } = this.state;
     return (
       <div className="app">
         <form action="">
-          <PersonalDetails />
+          <PersonalDetails onChange={this.personalDetailsChange} />
         </form>
-        <Resume />
+        <Resume personalDetails={personalDetails} />
       </div>
     );
   }
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,7 +2,15 @@ import React, { Component } from "react";
 import "../styles/Resume.css";
 class Resume extends Component {
   render() {
-    return <div className="resume">resume resume</div>;
+    const { email, phoneNumber, fullName, address } =
+      this.props.personalDetails;
+    return (
+      <div className="resume">
+        <div className="personal-info section">
+          <p>{email}</p>
+        </div>
+      </div>
+    );
   }
 }
 
