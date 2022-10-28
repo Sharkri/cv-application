65f79a9e80452024b5093d9c7c49c9d1b5291a6f
Sharkri <sharkdude955@gmail.com>
2022-10-28 04:42:25 -0500

    fix no-useless-constructor

--- src/components/InputGroup.js
+++ src/components/InputGroup.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 import "../styles/InputGroup.css";
 class InputGroup extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     const {
       id,
