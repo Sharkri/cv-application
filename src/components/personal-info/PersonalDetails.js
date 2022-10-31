9a7a035cd0a571778e99d62863d3904bbc32c6c7
Sharkri <sharkdude955@gmail.com>
2022-10-30 23:52:32 -0500

    refactored personal-info

--- src/components/personal-info/PersonalDetails.js
+++ src/components/personal-info/PersonalDetails.js
@@ -1,55 +1,51 @@
-import React, { Component } from "react";
+import React from "react";
 import InputGroup from "../InputGroup";
 import "../../styles/PersonalDetails.css";
 
-class PersonalDetails extends Component {
-  render() {
-    const { onChange, email, fullName, phoneNumber, address } = this.props;
-    return (
-      <form className="personal-details">
-        <h2>Personal Details</h2>
-        <InputGroup
-          type="text"
-          id="full-name"
-          labelText="Full name"
-          placeholder="First and last name"
-          value={fullName}
-          onChange={onChange}
-          data-key="fullName"
-        />
-        <InputGroup
-          type="email"
-          id="email"
-          labelText="Email"
-          placeholder="Enter email"
-          value={email}
-          onChange={onChange}
-          data-key="email"
-          recommended
-        />
-        <InputGroup
-          type="tel"
-          id="phone-number"
-          labelText="Phone number"
-          placeholder="Enter phone number"
-          value={phoneNumber}
-          onChange={onChange}
-          data-key="phoneNumber"
-          recommended
-        />
-        <InputGroup
-          type="text"
-          id="address"
-          labelText="Address"
-          placeholder="City, Country"
-          value={address}
-          onChange={onChange}
-          data-key="address"
-          recommended
-        />
-      </form>
-    );
-  }
+function PersonalDetails({ onChange, email, fullName, phoneNumber, address }) {
+  return (
+    <form className="personal-details">
+      <h2>Personal Details</h2>
+      <InputGroup
+        type="text"
+        id="full-name"
+        labelText="Full name"
+        placeholder="First and last name"
+        value={fullName}
+        onChange={onChange}
+        data-key="fullName"
+      />
+      <InputGroup
+        type="email"
+        id="email"
+        labelText="Email"
+        placeholder="Enter email"
+        value={email}
+        onChange={onChange}
+        data-key="email"
+        recommended
+      />
+      <InputGroup
+        type="tel"
+        id="phone-number"
+        labelText="Phone number"
+        placeholder="Enter phone number"
+        value={phoneNumber}
+        onChange={onChange}
+        data-key="phoneNumber"
+        recommended
+      />
+      <InputGroup
+        type="text"
+        id="address"
+        labelText="Address"
+        placeholder="City, Country"
+        value={address}
+        onChange={onChange}
+        data-key="address"
+        recommended
+      />
+    </form>
+  );
 }
-
 export default PersonalDetails;
--- src/components/personal-info/PersonalInfoSection.js
+++ src/components/personal-info/PersonalInfoSection.js
@@ -1,37 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 import "../../styles/PersonalInfoSection.css";
 
-class PersonalInfoSection extends Component {
-  render() {
-    const { email, fullName, phoneNumber, address } = this.props;
-    return (
-      <div className="personal-info">
-        <h1 className="resume-name">{fullName}</h1>
-        <div className="contact-info">
-          {email && (
-            <div>
-              <i className="fa-solid fa-envelope" />
-              <span>{email}</span>
-            </div>
-          )}
+function PersonalInfoSection({ email, fullName, phoneNumber, address }) {
+  return (
+    <div className="personal-info">
+      <h1 className="resume-name">{fullName}</h1>
+      <div className="contact-info">
+        {email && (
+          <div>
+            <i className="fa-solid fa-envelope" />
+            <span>{email}</span>
+          </div>
+        )}
 
-          {phoneNumber && (
-            <div>
-              <i className="fa-solid fa-phone" />
-              <span>{phoneNumber}</span>
-            </div>
-          )}
+        {phoneNumber && (
+          <div>
+            <i className="fa-solid fa-phone" />
+            <span>{phoneNumber}</span>
+          </div>
+        )}
 
-          {address && (
-            <div>
-              <i className="fa-solid fa-location-dot" />
-              <span>{address}</span>
-            </div>
-          )}
-        </div>
+        {address && (
+          <div>
+            <i className="fa-solid fa-location-dot" />
+            <span>{address}</span>
+          </div>
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default PersonalInfoSection;
