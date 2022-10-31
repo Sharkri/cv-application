8cc43365bd2a77fcedf4e211a099f79840cf16a7
Sharkri <sharkdude955@gmail.com>
2022-10-30 23:33:12 -0500

    converted Buttons and CollapsedForm to functional components

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,27 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import "../styles/Buttons.css";
 
-class Buttons extends Component {
-  render() {
-    const { cancel, save, remove } = this.props;
+function Buttons(props) {
+  const { cancel, save, remove } = props;
 
-    return (
-      <div className="buttons">
-        <button className="delete" onClick={remove} type="button">
-          <i className="fa-solid fa-trash" />
-          Delete
+  return (
+    <div className="buttons">
+      <button className="delete" onClick={remove} type="button">
+        <i className="fa-solid fa-trash" />
+        Delete
+      </button>
+      <div className="main-buttons">
+        <button className="cancel" onClick={cancel} type="button">
+          Cancel
         </button>
-        <div className="main-buttons">
-          <button className="cancel" onClick={cancel} type="button">
-            Cancel
-          </button>
-          <button className="save" onClick={save} type="submit">
-            Save
-          </button>
-        </div>
+        <button className="save" onClick={save} type="submit">
+          Save
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Buttons;
--- src/components/CollapsedForm.js
+++ src/components/CollapsedForm.js
@@ -1,28 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import "../styles/CollapsedForm.css";
 
-class CollapsedForm extends Component {
-  render() {
-    const { onClick, hideForm, title, arrayName } = this.props;
-    const { isHidden, id } = this.props.form;
-    return (
-      <button
-        className="collapsed-form section-form"
-        id={id}
-        onClick={onClick}
-        data-array-name={arrayName}
-      >
-        <p className="collapsed-form-title">{title}</p>
-        <i
-          className={`fa-regular ${isHidden ? "fa-eye-slash" : "fa-eye"} eye`}
-          onClick={(e) => {
-            e.stopPropagation();
-            hideForm(e);
-          }}
-        ></i>
-      </button>
-    );
-  }
+function CollapsedForm(props) {
+  const { onClick, hideForm, title, arrayName } = props;
+  const { isHidden, id } = props.form;
+  return (
+    <button
+      className="collapsed-form section-form"
+      id={id}
+      onClick={onClick}
+      data-array-name={arrayName}
+    >
+      <p className="collapsed-form-title">{title}</p>
+      <i
+        className={`fa-regular ${isHidden ? "fa-eye-slash" : "fa-eye"} eye`}
+        onClick={(e) => {
+          e.stopPropagation();
+          hideForm(e);
+        }}
+      ></i>
+    </button>
+  );
 }
 
 export default CollapsedForm;
